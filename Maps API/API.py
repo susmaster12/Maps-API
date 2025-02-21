398fb3239eee4a1c49467f0953410d61b676e5a7
ABIO <t0pnut@yandex.com>
2025-02-21 20:11:21 +0300

    стрелки влияют на маштаб w, a, s, d  передвижение

--- Maps API/API.py
+++ Maps API/API.py
@@ -3,6 +3,7 @@ import pygame
 import requests
 from geopy.distance import geodesic
 
+
 def make_mapBr(server_address_maps, map_params):
     response = requests.get(server_address_maps, map_params)
     if not response:
@@ -13,8 +14,24 @@ def make_mapBr(server_address_maps, map_params):
     return map_file
 
 
-toponym_to_find = " ".join(sys.argv[1:])
-ll, spn = toponym_to_find.split()
+def move(keys, ll, spn):
+    if keys[pygame.K_UP] and float(spn.split(",")[0]) < 2:
+        spn = ",".join([str(float(spn.split(",")[0]) + 0.01), str(float(spn.split(",")[1]) + 0.01)])
+    if keys[pygame.K_DOWN] and float(spn.split(",")[0]) > 0.001:
+        spn = ",".join([str(float(spn.split(",")[0]) - 0.01), str(float(spn.split(",")[1]) - 0.01)])
+    if keys[pygame.K_d]:
+        ll = ",".join([str(float(ll.split(",")[0]) + (float(spn.split(",")[0]))), str(ll.split(",")[1])])
+    if keys[pygame.K_a]:
+        ll = ",".join([str(float(ll.split(",")[0]) - (float(spn.split(",")[0]))), str(ll.split(",")[1])])
+    if keys[pygame.K_w]:
+        ll = ",".join([str(ll.split(",")[0]), str(float(ll.split(",")[1]) + (float(spn.split(",")[0])))])
+    if keys[pygame.K_s]:
+        ll = ",".join([str(ll.split(",")[0]), str(float(ll.split(",")[1]) - (float(spn.split(",")[0])))])
+    return ll, spn
+
+
+ll = "37.677751,55.757718"
+spn = "0.01,0.01"
 STATIC_MAPS_KEY = 'ef67d706-4387-4517-8b08-50f4c0929dd7'
 map_params = {
     "ll": ll,
@@ -30,7 +47,15 @@ if map_file:
     screen = pygame.display.set_mode((600, 400))
     screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
-
     while pygame.event.wait().type != pygame.QUIT:
-        pass
-    pygame.quit()
+        keys = pygame.key.get_pressed()
+        ll, spn = move(keys, ll, spn)
+        map_params = {
+            "ll": ll,
+            "spn": spn,
+            "apikey": STATIC_MAPS_KEY,
+        }
+        map_file = make_mapBr(server_address_maps, map_params)
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        pygame.display.flip()  # Обновление экрана
+pygame.quit()
